c051f5a9984aea6a60cda520a8c2fa32154676ab
Tom Green <tom@chaoscube.xyz>
2016-03-09 23:46:31 +0000

    Moved to CB SDK 2.2.5 for dependency. Add an initial sub-doc based end point

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,10 +2,12 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.couchbase.client.core.message.kv.subdoc.multi.Lookup;
 import com.couchbase.client.java.AsyncBucket;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.subdoc.DocumentFragment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.couchbase.core.CouchbaseTemplate;
@@ -36,22 +38,32 @@ public class GreetingController {
         return new Greeting(String.valueOf(counter.incrementAndGet()), String.format(template, name));
     }
 
-    @Bean
     @RequestMapping("/cbgreeting")
     public Greeting cbgreeting() {
-        JsonDocument tomTest = cbBucket.get("tomtest");
+        JsonDocument tomTest = cbBucket.get("mufc");
 
         return new Greeting(
                 String.valueOf(counter.incrementAndGet()),
                String.format(template, tomTest.content().get("name")));
     }
 
+    @RequestMapping("/cbgreetingsubdoc")
+    public Greeting cbgreetingsubdoc() {
+        DocumentFragment<Lookup> tomTest = cbBucket
+                .lookupIn("mufc")
+                .get("name")
+                .doLookup();
+
+        return new Greeting(
+                String.valueOf(counter.incrementAndGet()),
+               String.format(template, tomTest.content("name", String.class)));
+    }
 
     @RequestMapping("/asyncgreeting")
     public Observable<Greeting> asyncgreeting(@RequestParam(value="name", defaultValue="world") String name) {
         AsyncBucket asyncBucket = cbBucket.async();
 
-        Observable<Greeting> tomTest = asyncBucket.get("tomtest")
+        Observable<Greeting> tomTest = asyncBucket.get("mufc")
             .flatMap( new Func1<JsonDocument, Observable<Greeting>>() {
                 @Override
                 public Observable<Greeting> call(JsonDocument gotDoc) {
